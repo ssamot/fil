0f96688e7b93938be0284d55b91ecb3680477d12
David Milec <milecdav@fel.cvut.cz>
2025-07-04 16:46:25 +0200

    Results saving for MCCFR

--- mains/main_mccfr_external.py
+++ mains/main_mccfr_external.py
@@ -8,24 +8,30 @@ from open_spiel.python.algorithms import exploitability
 from open_spiel.python.algorithms import external_sampling_mccfr
 
 import tqdm
+import json
 
 def main(unused_argv):
     np.random.seed(0)
     game = pyspiel.load_game("leduc_poker")
     es_solver = external_sampling_mccfr.ExternalSamplingSolver(game, external_sampling_mccfr.AverageType.SIMPLE)
-    cfr_iterations = 1000
+    cfr_iterations = 180000
+    results = {}
     with tqdm.tqdm(range(cfr_iterations)) as pbar:
         for i in range(cfr_iterations):
             es_solver.iteration()
             if((i%100) == 0):
                 conv = exploitability.nash_conv(game, es_solver.average_policy())
-            pbar.update(1)
-
+                results[i] = conv
+            pbar.update(1)            
             pbar.set_description(f"Iteration {i}")
             pbar.set_postfix({"exploitability": f"{conv:.6f}"})
-
     conv = exploitability.nash_conv(game, es_solver.average_policy())
     print("Leduc2P, conv = {}".format(conv))
+    results[i] = conv
+
+    results_file = "./results/mccfr_leduc.json"
+    with open(results_file, 'w') as results_file:
+        json.dump(results, results_file)
 
 if __name__ == "__main__":
   app.run(main)
