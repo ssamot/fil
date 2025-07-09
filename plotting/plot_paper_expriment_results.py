d209b306b2fd68b21c31e2baca7c94442e6077ac
David Milec <milecdav@fel.cvut.cz>
2025-07-09 15:45:23 +0200

    Plotting prepared for the results to show the confidence intervals of mean from multiple runs

---
+++ plotting/plot_paper_expriment_results.py
@@ -0,0 +1,68 @@
+import json
+import matplotlib.pyplot as plt
+import sys
+import glob
+import numpy as np
+from scipy import stats
+
+pytorch_files = "results/leduc_pytorch_test*"
+lgbm_files = "results/leduc_lgb_test*"
+
+def load_data(file_start):
+    data = {"x": [], "y": []}
+    for file_name in glob.glob(file_start):
+        with open(file_name) as f:
+            d = json.load(f)
+            x = sorted([int(k) for k in d.keys()])
+            y = [d[str(i)] for i in x]
+            data["x"].append(x)
+            data["y"].append(y)
+    return data
+
+def check_data(data):
+    prev_x = None
+    for x, y in zip(data["x"], data["y"]):
+        if len(x) != len(y):
+            raise Exception("Values loaded incorectly, len(X) != len(y)")
+        if prev_x is not None:
+            if prev_x != x:
+                raise Exception("Different files have different iterations")
+        
+
+def load_and_plot():
+    pytorch_data = load_data(pytorch_files)
+    lgbm_data = load_data(lgbm_files)
+
+    check_data(pytorch_data)
+    check_data(lgbm_data)
+
+    pytorch_mean = np.mean(pytorch_data["y"], axis=0)
+    n = len(pytorch_data["y"])
+    sem = stats.sem(pytorch_data["y"], axis=0)  # shape: same as mean
+    confidence = 0.95
+    pytorch_ci = sem * stats.t.ppf((1 + confidence) / 2., df=n-1)
+
+    lgbm_mean = np.mean(lgbm_data["y"], axis=0)
+    n = len(lgbm_data["y"])
+    sem = stats.sem(lgbm_data["y"], axis=0)  # shape: same as mean
+    confidence = 0.95
+    lgbm_ci = sem * stats.t.ppf((1 + confidence) / 2., df=n-1)
+
+    plt.figure(figsize=(16,9))
+    
+    plt.plot(pytorch_data["x"][0], pytorch_mean, label="DeepCFRPytorch", color='b')
+    plt.fill_between(pytorch_data["x"][0], (pytorch_mean - pytorch_ci), (pytorch_mean + pytorch_ci), color='b', alpha=.1)
+    
+    plt.plot(lgbm_data["x"][0], lgbm_mean, label="DeepCFRLGBM", color='r')
+    plt.fill_between(lgbm_data["x"][0], (lgbm_mean - lgbm_ci), (lgbm_mean + lgbm_ci), color='r', alpha=.1)
+
+    plt.xlabel("Iteration")
+    plt.ylabel("Exploitability")
+    plt.title("Exploitability over Iterations")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig("results/fig.png", dpi=300)
+
+if __name__ == "__main__":
+    load_and_plot()
