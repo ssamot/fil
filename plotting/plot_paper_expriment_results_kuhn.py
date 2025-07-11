19be7b3e3e63ee927260c5b5ed382d973c557749
David Milec <milecdav@fel.cvut.cz>
2025-07-11 11:54:59 +0200

    Reworked plotting so that I can change all the plots configs at once

--- plotting/plot_paper_expriment_results_kuhn.py
+++ plotting/plot_paper_expriment_results_kuhn.py
@@ -1,75 +1,19 @@
-import json
-import matplotlib.pyplot as plt
 import sys
-import glob
-import numpy as np
-from scipy import stats
+from plot_paper_expriment_general import load_data, load_and_plot
 
 pytorch_files = "results/kuhn_pytorch*"
 lgbm_files = "results/kuhn_lgb*"
 
-def load_data(file_start):
-    data = {"x": [], "y": []}
-    for file_name in glob.glob(file_start):
-        with open(file_name) as f:
-            d = json.load(f)
-            x = sorted([int(k) for k in d.keys()])
-            y = [d[str(i)] for i in x]
-            data["x"].append(x)
-            data["y"].append(y)
-    return data
-
-def check_data(data):
-    prev_x = None
-    for x, y in zip(data["x"], data["y"]):
-        if len(x) != len(y):
-            raise Exception("Values loaded incorectly, len(X) != len(y)")
-        if prev_x is not None:
-            if prev_x != x:
-                raise Exception("Different files have different iterations")
-        
-
-def load_and_plot(use_log_scale):
+def load(use_log_scale):
     pytorch_data = load_data(pytorch_files)
     lgbm_data = load_data(lgbm_files)
 
-    check_data(pytorch_data)
-    check_data(lgbm_data)
+    data = [
+        {"name": "DeepCFR", "data": pytorch_data, "color" : "blue"},
+        {"name": "LGBM", "data": lgbm_data, "color" : "orange"},
+    ]
 
-    pytorch_mean = np.mean(pytorch_data["y"], axis=0)
-    n = len(pytorch_data["y"])
-    sem = stats.sem(pytorch_data["y"], axis=0)  # shape: same as mean
-    confidence = 0.95
-    pytorch_ci = sem * stats.t.ppf((1 + confidence) / 2., df=n-1)
-
-    lgbm_mean = np.mean(lgbm_data["y"], axis=0)
-    n = len(lgbm_data["y"])
-    sem = stats.sem(lgbm_data["y"], axis=0)  # shape: same as mean
-    confidence = 0.95
-    lgbm_ci = sem * stats.t.ppf((1 + confidence) / 2., df=n-1)
-
-    plt.figure(figsize=(6,4.5))
-
-    if use_log_scale:
-        plt.yscale("log")
-    
-    plt.plot(pytorch_data["x"][0], pytorch_mean, label="DeepCFRPytorch", color='b')
-    plt.fill_between(pytorch_data["x"][0], (pytorch_mean - pytorch_ci), (pytorch_mean + pytorch_ci), color='b', alpha=.1)
-    
-    plt.plot(lgbm_data["x"][0], lgbm_mean, label="DeepCFRLGBM", color='r')
-    plt.fill_between(lgbm_data["x"][0], (lgbm_mean - lgbm_ci), (lgbm_mean + lgbm_ci), color='r', alpha=.1)
-
-    plt.plot([lgbm_data["x"][0][0], lgbm_data["x"][0][-1]], [0.9166666666666666]*2, label="Random", color="black")
-
-    plt.xlabel("Iteration")
-    plt.ylabel("Exploitability")
-    plt.legend()
-    plt.grid(True)
-    plt.tight_layout()
-    if use_log_scale:
-        plt.savefig("results/fig_kuhn_log.pdf", dpi=300)
-    else:
-        plt.savefig("results/fig_kuhn.pdf", dpi=300)
+    load_and_plot(data, 0.9166666666666666, "kuhn", use_log_scale)
 
 if __name__ == "__main__":
     args = sys.argv[1:]
@@ -79,4 +23,4 @@ if __name__ == "__main__":
         use_log = True
         args.remove("--log")
 
-    load_and_plot(use_log)
+    load(use_log)
--- plotting/plot_paper_expriment_results_leduc.py
+++ plotting/plot_paper_expriment_results_leduc.py
@@ -1,37 +1,12 @@
-import json
-import matplotlib.pyplot as plt
 import sys
-import glob
-import numpy as np
-from scipy import stats
+from plot_paper_expriment_general import load_data, load_and_plot
 
 pytorch_files = "results/leduc_pytorch*"
 lgbm_files = "results/leduc_lgb*"
 pt_splines_files = "results/leduc_pt_splines*"
 pt_dt_files = "results/leduc_pt_dt*"
 
-def load_data(file_start):
-    data = {"x": [], "y": []}
-    for file_name in glob.glob(file_start):
-        with open(file_name) as f:
-            d = json.load(f)
-            x = sorted([int(k) for k in d.keys()])
-            y = [d[str(i)] for i in x]
-            data["x"].append(x)
-            data["y"].append(y)
-    return data
-
-def check_data(data):
-    prev_x = None
-    for x, y in zip(data["x"], data["y"]):
-        if len(x) != len(y):
-            raise Exception("Values loaded incorectly, len(X) != len(y)")
-        if prev_x is not None:
-            if prev_x != x:
-                raise Exception("Different files have different iterations")
-        
-
-def load_and_plot(use_log_scale):
+def load(use_log_scale):
     pytorch_data = load_data(pytorch_files)
     lgbm_data = load_data(lgbm_files)
     pt_splines_data = load_data(pt_splines_files)
@@ -44,38 +19,7 @@ def load_and_plot(use_log_scale):
         {"name": "Public table decision tree", "data": pt_dt_data, "color" : "red"}
     ]
 
-    for d in data:
-        check_data(d["data"])
-
-        data_mean = np.mean(d["data"]["y"], axis=0)
-        n = len(d["data"]["y"])
-        sem = stats.sem(d["data"]["y"], axis=0)  # shape: same as mean
-        confidence = 0.95
-        ci = sem * stats.t.ppf((1 + confidence) / 2., df=n-1)
-        d["mean"] = data_mean
-        d["ci"] = ci
-
-    plt.figure(figsize=(6,4.5))
-
-    if use_log_scale:
-        plt.yscale("log")
-
-    for d in data:
-        plt.plot(d["data"]["x"][0], d["mean"], label=d["name"], color=d["color"])
-        plt.fill_between(d["data"]["x"][0], (d["mean"] - d["ci"]), (d["mean"] + d["ci"]), color=d["color"], alpha=.1)
-
-    plt.plot([lgbm_data["x"][0][0], lgbm_data["x"][0][-1]], [4.747222222222222]*2, label="Random", color="black")
-
-    plt.xlabel("Iteration")
-    plt.ylabel("Exploitability")
-    plt.title("Exploitability over Iterations")
-    plt.legend()
-    plt.grid(True)
-    plt.tight_layout()
-    if use_log_scale:
-        plt.savefig("results/fig_leduc_log.pdf", dpi=300)
-    else:
-        plt.savefig("results/fig_leduc.pdf", dpi=300)
+    load_and_plot(data, 4.747222222222222, "leduc", use_log_scale)
 
 if __name__ == "__main__":
     args = sys.argv[1:]
@@ -85,4 +29,4 @@ if __name__ == "__main__":
         use_log = True
         args.remove("--log")
 
-    load_and_plot(use_log)
+    load(use_log)
